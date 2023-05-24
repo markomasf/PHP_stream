fd92f3b7c8cbac09d8dec7970782118e9c0d9f66
Marco Antônio <markomasf@gmail.com>
2023-05-24 08:34:26 -0300

    Caracteres UTF 8 para Windows 1252 CSV

--- arquivos-para-csv.php
+++ arquivos-para-csv.php
@@ -6,12 +6,12 @@ $outrosCursos = file('cursos-php.txt');
 $arquivoCsv = fopen('cursos.csv', 'w');
 
 foreach ($meusCursos as $curso){
-    $linha = [trim($curso), 'Sim'];
+    $linha = [trim(mb_convert_encoding($curso, "Windows-1252","UTF-8")), 'Sim'];
     fputcsv($arquivoCsv, $linha, ';');
 }
 
 foreach ($outrosCursos as $curso){
-    $linha = [trim($curso), 'Não'];
+    $linha = [trim(mb_convert_encoding($curso, "Windows-1252","UTF-8")), 'Não'];
     fputcsv($arquivoCsv, $linha, ';');
 }
 
--- ls.php
+++ ls.php
@@ -2,6 +2,8 @@
 
 $diretorioAtual = dir('.');
 
+echo $diretorioAtual->path . PHP_EOL;
+
 while($arquivo = $diretorioAtual->read()){
     echo $arquivo . PHP_EOL;
 }
