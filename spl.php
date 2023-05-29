4203b3ebfa55f0cdf26b0f138ee70ae6ee4dd692
Marco Antônio <markomasf@gmail.com>
2023-05-29 10:16:22 -0300

    spl

--- spl.php
+++ spl.php
@@ -5,5 +5,5 @@ $arquivoCursos = new SplFileObject('cursos.csv');
 while (!$arquivoCursos->eof()){
     $linha = $arquivoCursos->fgetcsv(';');
 
-    echo $linha[0] . PHP_EOL;
+    echo mb_convert_encoding($linha[0], "UTF-8", "Windows-1252") . PHP_EOL;
 }
